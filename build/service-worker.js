6a3ab97c2ec4ad542d252bf0c0cb61f313c4d9c2
Azrie Bakri <azrie.bakri@servicerocket.com>
2021-05-25 10:23:37 +0800

    add portfolio page

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6eec3c919fe58e8207e4ee3555d112a5.js"
+  "/precache-manifest.5e1a321f0d527909957dc69ab1aae3d5.js"
 );
 
 self.addEventListener('message', (event) => {
